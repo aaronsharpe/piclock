698e703dd26956238a22bf3f25797e96f48cc6f4
aaronsharpe <aaronlsharpe@gmail.com>
2020-08-08 17:12:49 -0700

    init spotify_state differently

--- piclock.py
+++ piclock.py
@@ -304,7 +304,8 @@ def main():
     # Initial fetching
     with open('.api_info.json', 'r') as f:
         api_info = json.load(f)
-    api_info, spotify_state = loop.run_until_complete(fetch_spotify(api_info))
+    spotify_state = {'is_playing': False, 'artist': '', 'song_title': ''}
+    #api_info, spotify_state = loop.run_until_complete(fetch_spotify(api_info))
     clock_prev = time.strftime('%H:%M')
     clock_state['time'] = clock_prev
     clock_state['net_info'] = loop.run_until_complete(fetch_net_info())
